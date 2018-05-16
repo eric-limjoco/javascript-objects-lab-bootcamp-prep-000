1f39fcd77389ad8df4a91c8eb652f09407ce039b
ericlimjoco@gmail.com Limjoco <ericlimjoco@gmail.com>
2018-05-16 10:50:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var recipes = {foo: 'bar', baz: 'bang'}
 
 function updateObjectWithKeyAndValue(object, key, value){
-  var newObject = Object.assign({}, object, {key: value})
+  var newObject = Object.assign({}, object)
+  newObject[key] = value
   return newObject
 }
 
