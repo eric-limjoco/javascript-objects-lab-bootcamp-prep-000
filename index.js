ab59b263ee696f0d9e5c5034f571cd65cb3a4730
ericlimjoco@gmail.com Limjoco <ericlimjoco@gmail.com>
2018-05-16 10:49:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 
-function deleteObjectByKey(object, key){
+function deleteFromObjectByKey(object, key){
   var newObject = Object.assign({}, object)
   delete newObject[key]
   return newObject
